e85829c710c394a4c6fbc62bbcdf7a05a2d1c36f
Pkw <104768274+sudopkw@users.noreply.github.com>
2026-08-12 19:09:45 +0200

    Update config.fish

--- config.fish
+++ config.fish
@@ -139,6 +139,7 @@ function fish_greeting
         "Logged in as: $USER" \
         "1337 4 3/3|2, $USER!" \
         "No way! Is that $USER??!!11!!11!!" \
+        "Remember to update your config, $USER!" \
         "Access Authorized, Welcome back, $USER?"
 
     set -l quote ""$greetings[(random 1 (count $greetings))]
